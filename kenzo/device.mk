77120007cf0c8abf4ec54f78b5c8fdf070543c99
mathew <mathewdennis693@gmail.com>
2021-08-12 19:52:16 +0530

    added missing libs

---
+++ kenzo/device.mk
@@ -0,0 +1,75 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# call the proprietary setup
+$(call inherit-product-if-exists, vendor/xiaomi/kenzo/kenzo-vendor.mk)
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info_extcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_extcodec.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_wcd9326.xml
+
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/idc/uinput-fpc.idc:system/usr/idc/uinput-fpc.idc
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/Atmel_maXTouch_Touchscreen.kl:system/usr/keylayout/Atmel_maXTouch_Touchscreen.kl \
+    $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/msm8976-tashalite-snd-card_Button_Jack.kl:system/usr/keylayout/msm8976-tashalite-snd-card_Button_Jack.kl \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.kenzo.rc
+
+# Wifi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
+
+
+
+#--------ut----------------------
+
+
+
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ubuntu/70-kenzo.rules:system/halium/etc/udev/rules.d/70-kenzo.rules \
+    $(LOCAL_PATH)/ubuntu/config::system/halium/var/lib/lxc/android/config \
+    $(LOCAL_PATH)/ubuntu/android.conf::system/halium/etc/ubuntu-touch-session.d/android.conf \
+    $(LOCAL_PATH)/ubuntu/touch.pa:system/halium/etc/pulse/touch.pa \
+
+
+
+
+
+#missing dependenceess
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full \
+    libprotobuf-cpp-lite \
+    libnetutils \
+    libandroid \
+    power.default \
+    tls-padding \
+#--------ut----------------------
+
+# Inherit from msm8956-common
+$(call inherit-product, device/xiaomi/msm8956-common/msm8956.mk)
---
+++ msm8956-common/kenzopref/src/com/lineageos/settings/kenzopref/KenzoPrefSettings.java
@@ -0,0 +1,109 @@
+/*
+ *  LeEco Extras Settings Module
+ *  Made by @andr68rus 2017
+ */
+
+package com.lineageos.settings.kenzopref;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Date;
+
+import android.util.Log;
+import android.os.SystemProperties;
+import java.io.*;
+import android.widget.Toast;
+import android.preference.ListPreference;
+
+import com.lineageos.settings.kenzopref.R;
+
+public class KenzoPrefSettings extends PreferenceActivity implements OnPreferenceChangeListener {
+	private static final boolean DEBUG = false;
+	private static final String TAG = "KenzoPref";
+	private SwitchPreference mVolteFix;
+
+		private static final String SYSTEM_PROPERTY_VOLTE_FIX = "persist.volte.fix";
+        private Preference mKcalPref;
+
+    private Context mContext;
+    private SharedPreferences mPreferences;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.kenzo_settings);
+        mKcalPref = findPreference("kcal");
+                mKcalPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                     @Override
+                     public boolean onPreferenceClick(Preference preference) {
+                         Intent intent = new Intent(getApplicationContext(), DisplayCalibration.class);
+                         startActivity(intent);
+                         return true;
+                     }
+                });
+        mContext = getApplicationContext();
+		
+        mVolteFix = (SwitchPreference) findPreference(SYSTEM_PROPERTY_VOLTE_FIX);
+        if( mVolteFix != null ) {
+            mVolteFix.setChecked(SystemProperties.getBoolean(SYSTEM_PROPERTY_VOLTE_FIX, false));
+            mVolteFix.setOnPreferenceChangeListener(this);
+        }
+}
+
+    private void setEnable(String key, boolean value) {
+	if(value) {
+ 	      SystemProperties.set(key, "1");
+    	} else {
+    		SystemProperties.set(key, "0");
+    	}
+    	if (DEBUG) Log.d(TAG, key + " setting changed");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final String key = preference.getKey();
+        boolean value;
+        String strvalue;
+        if (DEBUG) Log.d(TAG, "Preference changed.");
+    	value = (Boolean) newValue;
+    	((SwitchPreference)preference).setChecked(value);
+    	setEnable(key,value);
+	return true;
+    }
+}
